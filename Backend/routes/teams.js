3d56e8327f81535475fab1283f98f9b38ff367f8
Evasive-6 <albinshiju285@gmail.com>
2025-04-01 16:26:45 +0530

    :fix:

--- Backend/routes/teams.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createTeam, getTeams } = require('../controllers/teamController');
-const auth = require('../middleware/auth');
-
-router.get('/', getTeams);
-
-module.exports = router;
